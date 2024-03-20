f6f9a247c2521c4f9db7bae9c5a732ee9c4d65a9
Diogo Jonsson <diogo.jonsson@hotmail.com>
2024-03-20 19:25:50 -0300

    views

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "notfound.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
