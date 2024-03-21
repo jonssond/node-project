b92fab40f13bf07ef12f90584cec84d94fbb8dcf
Diogo Jonsson <diogo.jonsson@hotmail.com>
2024-03-20 22:58:58 -0300

    template eng

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const rootDir = require("./util/path");
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(rootDir, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminData.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
